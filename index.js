e10bff3253baa3475d53331298faffd48bf67ae8
boydungbg <dunglc.nde18055@vtc.edu.vn>
2019-08-28 14:43:40 +0700

    aa

---
+++ index.js
@@ -0,0 +1,63 @@
+
+
+function submitButtonStyle(_this) {
+  _this.style.backgroundColor = "E8B119";
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+document.getElementById('button-right-click-top').addEventListener('click', () => {
+  document.getElementById('down-iconn').classList.toggle('transform-360');
+  console.log('loz');
+})
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+$(document).ready(function () {
+  $(".button-right-click1").bind("click", function () {
+      $(this).toggleClass("changebackground1");
+      $("#arrow1").toggleClass("rotate");
+  });
+  $("#button-right-click2").bind("click", function () {
+      $(this).toggleClass("changebackground2");
+      $("#arrow2").toggleClass("rotate");
+  });
+});
